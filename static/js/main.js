6a06211cc11ae1ad410577300deaa3ec857cb253
Andres <brinezlopez08@gmail.com>
2022-12-18 14:28:21 -0500

    Mostrar las  imagenes en el slider y traer informacion API

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,47 @@
-console.log(conexion('movie/popular').then(
-    data => console.log(data)
-    ))
+import { slider } from "./slider.js";
+
+const  containerSlider= document .querySelector('.slideshow-container' );
+
+
+function getPopularMovies(){
+    conexion('movie/popular').then((data) => {
+        containerSlider.innerHTML = ''
+        const peliculas = data.results
+        console.log(data)
+        
+        console.log(peliculas)
+        peliculas.forEach(pelicula => {
+            const {poster_path, title, id} = pelicula
+            const url = `https://image.tmdb.org/t/p/w500${poster_path}`
+
+            const div = document.createElement('div')
+            div.classList.add('mySlides')
+            div.classList.add('fade')
+            div.innerHTML = `
+                <img src="${url}"class="slider-img" >
+            `            
+            containerSlider.appendChild(div)
+        
+                
+        });
+
+        const controller=`
+        <a class="prev">&#10094;</a>
+        <a class="next">&#10095;</a>
+        `
+        containerSlider.insertAdjacentHTML('beforeend', controller)
+
+        const prevIcon = document.getElementsByClassName('prev')[0];
+        const nextIcon = document.getElementsByClassName('next')[0];
+        
+        slider(prevIcon, nextIcon)
+
+    })
+    
+    
+    
+}
+
+getPopularMovies()
+
+
