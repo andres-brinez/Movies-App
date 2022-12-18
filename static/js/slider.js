d259d4da82fc984e83430a4843345aba1f6b347e
Andres <brinezlopez08@gmail.com>
2022-12-18 14:27:55 -0500

    cambios para  que funcionen los controller

--- static/js/slider.js
+++ static/js/slider.js
@@ -1,56 +1,62 @@
-var slideIndex = 1;
-showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length)
-   {
-    slideIndex = 1
-  } 
-  if (n < 1) {
-    slideIndex = slides.length
+export function slider (prevIcon, nextIcon){
+  
+    var slideIndex = 1;
+    showSlides(slideIndex);
+  
+    // Next/previous controls
+    function plusSlides(n) {
+    showSlides(slideIndex += n);
+    }
+  
+    prevIcon.addEventListener('click', () => {
+    plusSlides(-1)
+    })
+  
+    nextIcon.addEventListener('click', () => {
+        plusSlides(1)
+    })
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
   }
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+   
+    if (n > slides.length)
+     {
+      slideIndex = 1
+    } 
+    if (n < 1) {
+      slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+   
+    slides[slideIndex-1].style.display = "block"; 
+  
+    
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+  
+  // mover los slides automaticamente
+  var myIndex = 0;
+  carousel();
+  
+  function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 3000); // Change image every 2 seconds
   }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
-
   
 }
-
-// mover los slides automaticamente
-var myIndex = 0;
-carousel();
-
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-    console.log(i)
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 3000); // Change image every 2 seconds
-}
-
-
-
-
