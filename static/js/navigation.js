e1291acc6d3854d67a4bf073a432bc6b8aaec37e
Andres <brinezlopez08@gmail.com>
2022-12-19 21:58:14 -0500

    Location y hash navigation

---
+++ static/js/navigation.js
@@ -0,0 +1,39 @@
+/* Esto para evitar llamar a las funciones automáticamente para que se ejecuten, 
+sino que lo podamos borrar en el futuro y podamos en el file de navigation.js, 
+llamarlas cuando location y hashchange nos avisen que estamos en la vista principal o en alguna otra vista */
+
+function navigation() {
+    console.log('navigation');
+    console.log(location.hash);
+
+    // obtener cantidad de letras
+
+
+    
+
+    if (location.hash.startsWith("#trends")){
+        console.log('trends');
+    }
+
+    else if (location.hash.startsWith("#search")){
+        console.log('search');
+    }
+
+    else if (location.hash.startsWith("#movie")){
+        console.log('movie');
+    }
+
+    else{
+        console.log('home');
+    }
+
+
+}
+
+/* este evento no solo lo queremos llamar cuando  cambie el hash, 
+tambien lo queremos llamar cuando cargue nuestra aplicación, la primera carga, 
+ no solo vamos a cambiar a navigator cuando cambie el hash sino tambien cuando cargue la aplicación */
+
+window.addEventListener('load', navigation,false);
+window.addEventListener('hashchange', navigation,false);
+
