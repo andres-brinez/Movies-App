b8468a594b9e81085f5f7ea554d2e272184c6e0e
Andres <brinezlopez08@gmail.com>
2022-12-19 21:15:03 -0500

    Obtener peliculas trending

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,14 @@
 import { slider } from "./slider.js";
 
 const  containerSlider= document .querySelector('.slideshow-container' );
+const  containerTrending= document .querySelector('.snap' );
 
 
 function getPopularMovies(){
     conexion('movie/popular').then((data) => {
         containerSlider.innerHTML = ''
         const peliculas = data.results
-        console.log(data)
+        
         
         console.log(peliculas)
         peliculas.forEach(pelicula => {
@@ -42,6 +43,28 @@ function getPopularMovies(){
     
 }
 
+function getMoviesTrending(){
+    conexion('trending/movie/day').then((data) => {
+        console.log(data)
+        containerTrending.innerHTML = ''
+        const peliculas = data.results
+        peliculas.forEach(pelicula => {
+            const {poster_path, title, id} = pelicula
+            const url = `https://image.tmdb.org/t/p/w500${poster_path}`
+
+            const div = document.createElement('div')
+            div.classList.add('caroulsel')
+            // agregar el id a la pelicula
+            div.setAttribute('id', id)
+            div.innerHTML = `<img src="${url}" alt="Imagen ${title}">` 
+
+            containerTrending.appendChild(div)
+        
+        })
+
+    })
+
+}
+
 getPopularMovies()
-
-
+getMoviesTrending()
