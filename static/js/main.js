167e4ac02b7c36eb93b953582fa810bbf4275c23
Andres <brinezlopez08@gmail.com>
2022-12-23 13:27:35 -0500

    Crear código del trailer de la  pelicula

--- static/js/main.js
+++ static/js/main.js
@@ -306,6 +306,31 @@ async function  getMovieDetails(id){
 
             
         })
+
+        // TRAILER
+
+        conexion(`movie/${id}/videos`).then((data)=>{
+            const trailer = data.results
+            
+            if (trailer.length<=0){
+                console.log('no hay trailer')
+            }
+
+            else{
+                const div = document.createElement('div')
+                div.classList.add('container-trailer')
+                div.innerHTML = `
+                <h2 id="title-trailer">Trailer</h2>
+                <div class="trailer">
+                    <iframe width="80%" height="500" src="https://www.youtube.com/embed/${trailer[0].key}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                `
+                containerDetailsMovie.appendChild(div)
+            }
+
+            
+            
+        })
     })
 }
 
