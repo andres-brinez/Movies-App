bd3379bc4f5225e2b267a8366739b9b4b100bca5
Andres Briñez <brinezlopez08@gmail.com>
2023-02-11 21:24:10 -0500

    agregar loaders

--- static/js/Home/structureHome.js
+++ static/js/Home/structureHome.js
@@ -16,8 +16,17 @@ function structureHome(){
         <div class="title-container">
             <h2 class="title">Trending Movies</h2>
         </div>
-        <div  class=" carousel container-trending snap"> </div>
-
+        <div  class=" carousel container-trending snap">
+            <div class="scrollLoading"></div>
+            <div class="scrollLoading"></div>
+            <div class="scrollLoading"></div>
+            <div class="scrollLoading"></div>
+            <div class="scrollLoading"></div>
+            <div class="scrollLoading"></div>
+            <div class="scrollLoading"></div>
+            <div class="scrollLoading"></div>
+            <div class="scrollLoading"></div>
+            <div class="scrollLoading"></div>
         </section>
 
         <section class="trending-tv">
@@ -26,6 +35,16 @@ function structureHome(){
             <h2 class="title">Trending TV</h2>
         </div>
         <div  class=" carousel container-trending-tv snap">
+            <div class="scrollLoading"></div>
+            <div class="scrollLoading"></div>
+            <div class="scrollLoading"></div>
+            <div class="scrollLoading"></div>
+            <div class="scrollLoading"></div>
+            <div class="scrollLoading"></div>
+            <div class="scrollLoading"></div>
+            <div class="scrollLoading"></div>
+            <div class="scrollLoading"></div>
+            <div class="scrollLoading"></div>
         </div>
 
         </section>
@@ -35,15 +54,29 @@ function structureHome(){
             <h2 class="title">Upcoming</h2>
         </div>
         <div   class="carousel container-upcoming snap">
+            <div class="scrollLoading"></div>
+            <div class="scrollLoading"></div>
+            <div class="scrollLoading"></div>
+            <div class="scrollLoading"></div>
+            <div class="scrollLoading"></div>
+            <div class="scrollLoading"></div>
+            <div class="scrollLoading"></div>
+            <div class="scrollLoading"></div>
+            <div class="scrollLoading"></div>
+            <div class="scrollLoading"></div>
 
         </div>
         </section>
     
         <section class="categories">
-        <h2>Categories</h2>
-        <div class="container-categories">
-        </div>
-        
+            <h2>Categories</h2>
+            <div class="container-categories">
+                <p style="
+                text-align: center;
+                font-size: 45px;
+                margin-bottom: 50px;
+                ">Cargando ...</p>
+            </div>
         </section>
 
     </div>
