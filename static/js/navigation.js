b555fd35b38904ac120b32005a3452c6cb10f357
Andres Briñez <brinezlopez08@gmail.com>
2023-02-11 21:08:29 -0500

    cambiar la navegación para el home

--- static/js/navigation.js
+++ static/js/navigation.js
@@ -1,48 +1,49 @@
 /* Esto para evitar llamar a las funciones automáticamente para que se ejecuten, 
 sino que lo podamos borrar en el futuro y podamos en el file de navigation.js, 
 llamarlas cuando location y hashchange nos avisen que estamos en la vista principal o en alguna otra vista */
-import { HomePage,categoryPage,MoviePage,DetailsMoviePage,ProfilePage,SeriesPage,PersonPage } from "./main.js";
-
+// import { HomePage,categoryPage,MoviePage,DetailsMoviePage,ProfilePage,SeriesPage,PersonPage } from "./main.js";
+import  {Home } from "../components/Home.js" 
 
 
 function navigation() {
     
 
     if (location.hash.startsWith("#search")){
-        MoviePage()
+        // MoviePage()
+        pass
         
     }
 
-    else if (location.hash.startsWith("#movie") || location.hash.startsWith("#tv")){
-        DetailsMoviePage()
-    }
+    // else if (location.hash.startsWith("#movie") || location.hash.startsWith("#tv")){
+    //     DetailsMoviePage()
+    // }
  
 
-    else if (location.hash.startsWith("#profile")){
-        console.log('profile');
-        ProfilePage()
-    }
+    // else if (location.hash.startsWith("#profile")){
+    //     console.log('profile');
+    //     ProfilePage()
+    // }
 
 
-    else if (location.hash.startsWith("#category")){
-        console.log('category');
-        categoryPage()
-    }
+    // else if (location.hash.startsWith("#category")){
+    //     console.log('category');
+    //     categoryPage()
+    // }
 
-    else if (location.hash.startsWith("#series/popular")){
-        console.log('tv/popular');
-        SeriesPage()
-    }
+    // else if (location.hash.startsWith("#series/popular")){
+    //     console.log('tv/popular');
+    //     SeriesPage()
+    // }
 
-    else if (location.hash.startsWith("#person/popular")){
-        console.log('home');
-        PersonPage()
+    // else if (location.hash.startsWith("#person/popular")){
+    //     console.log('home');
+    //     PersonPage()
 
-    }
+    // }
 
     else{
         console.log('home');
-        HomePage()
+        Home()
 
     }
     
