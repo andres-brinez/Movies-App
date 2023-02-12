459028ba5b375dbf2ff49c1588af2ae2ad720463
Andres Briñez <brinezlopez08@gmail.com>
2023-02-11 21:09:52 -0500

    crea la estructura del home

---
+++ static/js/Home/structureHome.js
@@ -0,0 +1,54 @@
+function structureHome(){
+
+    main.innerHTML =`
+    <div class="container-Home">
+
+        <!-- Slideshow container -->
+        <div class="slideshow-container">
+        <div class="loadSlider">
+
+        </div>
+        </div>
+
+      
+        <section class="trending-movie">
+
+        <div class="title-container">
+            <h2 class="title">Trending Movies</h2>
+        </div>
+        <div  class=" carousel container-trending snap"> </div>
+
+        </section>
+
+        <section class="trending-tv">
+
+        <div class="title-container">
+            <h2 class="title">Trending TV</h2>
+        </div>
+        <div  class=" carousel container-trending-tv snap">
+        </div>
+
+        </section>
+
+        <section class="upcoming">
+        <div class="title-container">
+            <h2 class="title">Upcoming</h2>
+        </div>
+        <div   class="carousel container-upcoming snap">
+
+        </div>
+        </section>
+    
+        <section class="categories">
+        <h2>Categories</h2>
+        <div class="container-categories">
+        </div>
+        
+        </section>
+
+    </div>
+    `
+    
+}
+
+export  {structureHome}
